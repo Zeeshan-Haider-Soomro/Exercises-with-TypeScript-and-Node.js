453669867ed481e6e40346bc771d7353eefdb51c
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-04-18 03:24:05 +0500

    typescript\Exercises with Typescript and Node.js

---
+++ Exercise-19-Empty Error/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Think of something you could store in a array.
+let items = ['pakistan', 'karachi', 'sindhi'];
+let items2 = [];
+items2.push("pakistan");
+items2.push("palestine");
+items2.push("iran");
+items2.push("europe");
+console.log(items2);
--- Exercise-19-Empty Error/index.ts
+++ Exercise-19-Empty Error/index.ts
@@ -1,5 +1,11 @@
 
 // Think of something you could store in a array.
 let items :string[] = ['pakistan', 'karachi', 'sindhi'];
+let items2:string[] = [] ;
 
+items2.push("pakistan")
+items2.push("palestine")
+items2.push("iran")
+items2.push("europe")
 
+console.log(items2);
